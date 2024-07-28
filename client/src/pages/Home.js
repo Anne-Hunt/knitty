34e86fcc45c86c498fbb37cbb55f0a082cd557f3
Anne Hunt <acathunt@gmail.com>
2024-07-27 22:07:03 -0600

    updated vite settings

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { InstagramEmbed } from 'react-social-media-embed';
 
 const Home = () => {
